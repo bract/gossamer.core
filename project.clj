ebcb7e6d595f296ef818958ec7d061841386c183
Shantanu Kumar <kumar.shantanu@gmail.com>
2018-10-10 19:51:03 +0530

    bump version to 0.6.1-0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bract/gossamer.core "0.6.1-0.1.0-SNAPSHOT"
+(defproject bract/gossamer.core "0.6.1-0.1.0"
   :description "A micro web framework module for Bract"
   :url "https://github.com/bract/gossamer.core"
   :license {:name "Eclipse Public License"
